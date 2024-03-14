9d6d4bbda3818ee87e558c1d705fdbdb5fab0f7b
Mehak Alamgir <159041130+missmehakalamgir@users.noreply.github.com>
2024-03-14 21:57:55 +0500

    main.ts

---
+++ main.ts
@@ -0,0 +1,8 @@
+// BMI CALCULATORIN TYPESCRIPT
+
+let weightInKg = 70;
+let heightInMeters = 1.78;
+
+let bmi = weightInKg/ (heightInMeters * heightInMeters)
+
+console.log(`Mehak Alamgir Your BMI Is ${bmi}`)
